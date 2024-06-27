cda146057de344ecdcc91ea8cc3ff604677333dd
Kavyaalungal <142202840+Kavyaalungal@users.noreply.github.com>
2024-06-27 10:59:16 +0530

    Add files via upload

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TextField, Grid, FormControlLabel, Checkbox, Button,
   MenuItem, Select, InputLabel, Radio, RadioGroup, FormControl,
   Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,
-  AppBar, Toolbar, Typography, Box
+  AppBar, Toolbar, Typography, Box, Container, FormGroup
 } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
-import { CCard, CCardBody } from '@coreui/react';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Patient = () => {
+  const [selectedView, setSelectedView] = useState('Bill View');
   const patients = [
     { slNo: 1, labNo: 5749, name: "KRISHNA A S", age: 22, gender: "F", date: "26-Jun-2024", collectedAt: "", reference: "N Mohanan BSc MBBS MS", tests: ["COMPLETE HAEMOGRAM +", "HBsAg", "HIV-DUO (Ivth GENERATION T", "Anti HCV", "HAEMOGLOBIN (HB)"], specimen: "EDTA WHOLE ...", total: 1860, pendingAmt: 0 },
     { slNo: 2, labNo: 5748, name: "RABESHNA", age: 36, gender: "F", date: "26-Jun-2024", collectedAt: "", reference: "Vineetha S MBBS DGO FMAS", tests: ["HAEMOGLOBIN (HB)", "ANTI MULLARIAN HORMONE", "THYROID STIMULATING HORM..."], specimen: "SERUM", total: 1510, pendingAmt: 0 },
@@ -18,138 +18,157 @@ const Patient = () => {
   ];
 
   const filterLabels = ['Result Issued', 'On Processing', 'Time Over', 'Cancelled Invoice', 'Half Verified', 'Time Over Reminder'];
-
+  const handleViewChange = (event) => {
+    setSelectedView(event.target.value); // Update selected radio button state
+  };
+  const checkboxColors = ['#0073e6', '#ff9900', '#4caf50', '#f44336', '#9c27b0', '#ffc107'];
   return (
     <>
-      <AppBar position="static" style={{ backgroundColor: '' }}>
+      <AppBar position="static" style={{ backgroundColor: '#0c97e6' }}>
         <Toolbar>
           <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Patient Views
+            PATIENT VIEWS
           </Typography>
         </Toolbar>
       </AppBar>
-      <Box sx={{ p: 2 }}>
-        <CCard className="mb-4">
-          <CCardBody>
-            <Grid container spacing={2} alignItems="center">
-              <Grid item xs={12}>
-                <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-                  <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                    {filterLabels.map((label) => (
+      <Container maxWidth="lg">
+        <Box sx={{ p: 2 }}>
+          <Paper elevation={3}>
+            <Box p={2}>
+              <Grid container spacing={2} alignItems="center">
+                <Grid item xs={12}>
+                <FormGroup row style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                    {filterLabels.map((label, index) => (
                       <FormControlLabel
                         key={label}
-                        control={<Checkbox />}
+                        control={<Checkbox sx={{ color: checkboxColors[index % checkboxColors.length] }} size="small" />}
                         label={label}
                         style={{ marginRight: '10px' }}
                       />
                     ))}
-                  </div>
-                  <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                    <FormControl component="fieldset" style={{ marginRight: '10px' }}>
-                      <RadioGroup row>
-                        {['Bill View', 'Result View', 'Track View'].map(label => (
-                          <FormControlLabel
-                            key={label}
-                            control={<Radio />}
-                            label={label}
-                          />
-                        ))}
-                      </RadioGroup>
-                    </FormControl>
-                    <FormControl variant="outlined" size="small" style={{ marginRight: '10px' }}>
-                      <InputLabel>Sort By</InputLabel>
-                      <Select label="Sort By" defaultValue="">
-                        <MenuItem value="">None</MenuItem>
-                        <MenuItem value="date">Date</MenuItem>
-                        <MenuItem value="name">Name</MenuItem>
-                        <MenuItem value="amount">Amount</MenuItem>
-                      </Select>
-                    </FormControl>
-                    <FormControl variant="outlined" size="small" style={{ marginRight: '10px' }}>
-                      <InputLabel>Status</InputLabel>
-                      <Select label="Status" defaultValue="">
-                        <MenuItem value="">All</MenuItem>
-                        <MenuItem value="paid">Paid</MenuItem>
-                        <MenuItem value="unpaid">Unpaid</MenuItem>
-                      </Select>
-                    </FormControl>
-                    <FormControl variant="outlined" size="small" style={{ marginRight: '10px' }}>
-                      <InputLabel>Type</InputLabel>
-                      <Select label="Type" defaultValue="">
-                        <MenuItem value="">Any</MenuItem>
-                        <MenuItem value="urgent">Urgent</MenuItem>
-                        <MenuItem value="normal">Normal</MenuItem>
-                      </Select>
-                    </FormControl>
-                    <TextField
-                      id="start-date"
-                      label="Start Date"
-                      type="date"
-                      variant="outlined"
-                      size="small"
-                      InputLabelProps={{ shrink: true }}
-                      style={{ marginRight: '10px' }}
-                    />
-                    <TextField
-                      id="end-date"
-                      label="End Date"
-                      type="date"
-                      variant="outlined"
-                      size="small"
-                      InputLabelProps={{ shrink: true }}
-                      style={{ marginRight: '10px' }}
-                    />
-                    <Button variant="contained" color="primary">Refresh</Button>
-                  </div>
-                </div>
-              </Grid>
-            </Grid>
-            <Grid container spacing={2} alignItems="center" style={{ marginTop: '20px' }}>
-              {['Area', 'Corporate', 'Phone No', 'Referral', 'Sample Id', 'Department', 'IPOP', 'Specimen', 'Email', 'Lab No', 'Name', 'Payment', 'Tests'].map(label => (
-                <Grid item xs={12} sm={3} key={label}>
-                  <TextField
-                    id={label.toLowerCase().replace(' ', '-')}
-                    label={label}
-                    variant="outlined"
-                    size="small"
-                    fullWidth
-                    InputLabelProps={{ style: { fontSize: '14px' } }}
-                    style={{ marginBottom: '10px' }}
-                  />
+                  </FormGroup>
+                  <Grid container spacing={2} alignItems="center" style={{ marginTop: '10px' }}>
+                    <Grid item xs={12} sm={4} md={3} lg={2}>
+                    <FormControl component="fieldset">
+                        <RadioGroup row value={selectedView} onChange={handleViewChange}>
+                          {['Bill View', 'Result View', 'Track View'].map(label => (
+                            <FormControlLabel
+                              key={label}
+                              value={label}
+                              control={<Radio size="small" />}
+                              label={label}
+                              style={{ marginRight: '10px' }}
+                            />
+                          ))}
+                        </RadioGroup>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12} sm={4} md={3} lg={2}>
+                      <FormControl variant="outlined" size="small" fullWidth>
+                        <InputLabel>Sort By</InputLabel>
+                        <Select label="Sort By" defaultValue="">
+                          <MenuItem value="">None</MenuItem>
+                          <MenuItem value="date">Date</MenuItem>
+                          <MenuItem value="name">Name</MenuItem>
+                          <MenuItem value="amount">Amount</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12} sm={4} md={3} lg={2}>
+                      <FormControl variant="outlined" size="small" fullWidth>
+                        <InputLabel>Status</InputLabel>
+                        <Select label="Status" defaultValue="">
+                          <MenuItem value="">All</MenuItem>
+                          <MenuItem value="paid">Paid</MenuItem>
+                          <MenuItem value="unpaid">Unpaid</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12} sm={4} md={3} lg={2}>
+                      <FormControl variant="outlined" size="small" fullWidth>
+                        <InputLabel>Type</InputLabel>
+                        <Select label="Type" defaultValue="">
+                          <MenuItem value="">Any</MenuItem>
+                          <MenuItem value="urgent">Urgent</MenuItem>
+                          <MenuItem value="normal">Normal</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                      <TextField
+                        id="start-date"
+                        label="Start Date"
+                        type="date"
+                        variant="outlined"
+                        size="small"
+                        InputLabelProps={{ shrink: true }}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                      <TextField
+                        id="end-date"
+                        label="End Date"
+                        type="date"
+                        variant="outlined"
+                        size="small"
+                        InputLabelProps={{ shrink: true }}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={2}>
+                      <Button variant="contained" color="primary" fullWidth>Refresh</Button>
+                    </Grid>
+                  </Grid>
                 </Grid>
-              ))}
-            </Grid>
-            <TableContainer component={Paper} style={{ marginTop: '20px' }}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    {['Sl No', 'Lab No', 'Name', 'Date', 'Collected At', 'Reference', 'Tests', 'Specimen', 'Total', 'Pend Amt'].map(header => (
-                      <TableCell key={header}>{header}</TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {patients.map((patient, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{patient.slNo}</TableCell>
-                      <TableCell>{patient.labNo}</TableCell>
-                      <TableCell>{patient.name} ({patient.age} Years {patient.gender})</TableCell>
-                      <TableCell>{patient.date}</TableCell>
-                      <TableCell>{patient.collectedAt}</TableCell>
-                      <TableCell>{patient.reference}</TableCell>
-                      <TableCell>{patient.tests.join(', ')}</TableCell>
-                      <TableCell>{patient.specimen}</TableCell>
-                      <TableCell>{patient.total}</TableCell>
-                      <TableCell>{patient.pendingAmt}</TableCell>
+              </Grid>
+              <Grid container spacing={2} alignItems="center" style={{ marginTop: '20px' }}>
+                {['Area', 'Corporate', 'Phone No', 'Referral', 'Sample Id', 'Department', 'IPOP', 'Specimen', 'Email', 'Lab No', 'Name', 'Payment', 'Tests'].map(label => (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={label}>
+                    <TextField
+                      id={label.toLowerCase().replace(' ', '-')}
+                      label={label}
+                      variant="outlined"
+                      size="small"
+                      fullWidth
+                      InputLabelProps={{ style: { fontSize: '14px' } }}
+                      style={{ marginBottom: '10px' }}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+              <TableContainer component={Paper} style={{ marginTop: '20px' }}>
+                <Table>
+                  <TableHead>
+                    <TableRow style={{ backgroundColor: '#f0f0f0' }}> {/* Change background color here */}
+                      {['Sl No', 'Lab No', 'Name', 'Date', 'Collected At', 'Reference', 'Tests', 'Specimen', 'Total', 'Pend Amt'].map(header => (
+                        <TableCell key={header}>{header}</TableCell>
+                      ))}
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <ToastContainer />
-          </CCardBody>
-        </CCard>
-      </Box>
+                  </TableHead>
+                  <TableBody>
+                    {patients.map((patient, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{patient.slNo}</TableCell>
+                        <TableCell>{patient.labNo}</TableCell>
+                        <TableCell>{patient.name} ({patient.age} Years {patient.gender})</TableCell>
+                        <TableCell>{patient.date}</TableCell>
+                        <TableCell>{patient.collectedAt}</TableCell>
+                        <TableCell>{patient.reference}</TableCell>
+                        <TableCell>{patient.tests.join(', ')}</TableCell>
+                        <TableCell>{patient.specimen}</TableCell>
+                        <TableCell>{patient.total}</TableCell>
+                        <TableCell>{patient.pendingAmt}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <ToastContainer />
+            </Box>
+          </Paper>
+        </Box>
+      </Container>
     </>
   );
 };
