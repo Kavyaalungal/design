ebd2b163a7c32061c700530a05e2a362115c8b5c
Kavyaalungal <142202840+Kavyaalungal@users.noreply.github.com>
2024-07-12 17:06:39 +0530

    Add files via upload

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -81,103 +81,142 @@ const Bill = () => {
           </Grid>
         </Box>
         <hr />
-        <Grid item xs={12} sm={6}>
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} md={6}>
+          <Grid item xs={6} sm={6} md={6}>
             <FormControl component="fieldset">
               <RadioGroup row value={collectionType} onChange={(e) => setCollectionType(e.target.value)}>
-                <FormControlLabel value="lab" control={<Radio />} label="Lab Collection" />
-                <FormControlLabel value="purchase" control={<Radio />} label="Purchase" />
-                <FormControlLabel value="pharmacy" control={<Radio />} label="Pharmacy Collection" />
+                <FormControlLabel
+                  value="lab"
+                  control={<Radio size="small" />}
+                  label={<Typography variant="body2">Lab Collection</Typography>}
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 12 } }}
+                />
+                <FormControlLabel
+                  value="purchase"
+                  control={<Radio size="small" />}
+                  label={<Typography variant="body2">Purchase</Typography>}
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 12 } }}
+                />
+                <FormControlLabel
+                  value="pharmacy"
+                  control={<Radio size="small" />}
+                  label={<Typography variant="body2">Pharmacy Collection</Typography>}
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 12 } }}
+                />
               </RadioGroup>
             </FormControl>
           </Grid>
 
-          {/* <Grid item xs={12} sm={12} md={6}>
+          <Grid item xs={6} sm={6} md={6}>
             <FormControl component="fieldset">
               <RadioGroup row value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)}>
-                <FormControlLabel value="cash" control={<Radio />} label="Cash" />
-                <FormControlLabel value="cheque" control={<Radio />} label="Cheque" />
-                <FormControlLabel value="bhim" control={<Radio />} label="BHIM/UPI Online Payment" />
+                <FormControlLabel
+                  value="cash"
+                  control={<Radio size="small" />}
+                  label={<Typography variant="body2">Cash</Typography>}
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 12 } }}
+                />
+                <FormControlLabel
+                  value="cheque"
+                  control={<Radio size="small" />}
+                  label={<Typography variant="body2">Cheque</Typography>}
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 12 } }}
+                />
+                <FormControlLabel
+                  value="bhim"
+                  control={<Radio size="small" />}
+                  label={<Typography variant="body2">BHIM/UPI Online Payment</Typography>}
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 12 } }}
+                />
               </RadioGroup>
             </FormControl>
-          </Grid> */}
+          </Grid>
+<Grid item xs={12} sm={6}>
+    <TextField
+    id='corporate'
+    label='Corporate'
+    variant='outlined'
+    size='small'
+    fullWidth
+    InputLabelProps={{style:{fontSize:'16PX'}}}
+    />
 
+</Grid>
           <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label="From Date"
-              type="date"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              value={fromDate}
-              onChange={(e) => setFromDate(e.target.value)}
-              sx={{ border: '1px solid #ccc', borderRadius: 1 }}
-            />
+          
+          </Grid>
+
+          <Grid item xs={12} md={3}>
+          <TextField
+                id="dob"
+                label="Date of Birth"
+                type="date"
+                variant="outlined"
+                size="small"
+                fullWidth
+                InputLabelProps={{ shrink: true, style: { fontSize: '14px' } }}
+              />
+          </Grid>
+
+          <Grid item xs={12} md={9}>
+         
+          </Grid>
+          <Grid item xs={12} md={3}>
+          <TextField
+                id="dob"
+                label="Date of Birth"
+                type="date"
+                variant="outlined"
+                size="small"
+                fullWidth
+                InputLabelProps={{ shrink: true, style: { fontSize: '14px' } }}
+              />
+          </Grid>
+          <Grid item xs={12} md={3}>
+          <TextField
+                id="dob"
+                label="Date of Birth"
+                type="date"
+                variant="outlined"
+                size="small"
+                fullWidth
+                InputLabelProps={{ shrink: true, style: { fontSize: '14px' } }}
+              />
           </Grid>
 
           <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label="Up To Date"
-              type="date"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              value={toDate}
-              onChange={(e) => setToDate(e.target.value)}
-              sx={{ border: '1px solid #ccc', borderRadius: 1 }}
-            />
+          
           </Grid>
 
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label="Amount"
-              sx={{ border: '1px solid #ccc', borderRadius: 1 }}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleFormSubmit}
-              disabled={loading} // Disable button when loading
-            >
-              {loading ? <CircularProgress size={24} /> : 'Auto Allocate'}
-            </Button>
-          </Grid>
-
-          <Grid item xs={12}>
+          {/* <Grid item xs={12}>
             <Typography variant="h6" gutterBottom>Bill Details</Typography>
             <Box sx={{ border: '1px solid #ccc', borderRadius: 1, padding: 2 }}>
               <Grid container spacing={2}>
-                <Grid item xs={2}>Slno</Grid>
-                <Grid item xs={2}>Lab No</Grid>
-                <Grid item xs={2}>Date</Grid>
-                <Grid item xs={2}>Select</Grid>
-                <Grid item xs={2}>Patient</Grid>
-                <Grid item xs={2}>Amount</Grid>
-                <Grid item xs={2}>Balance</Grid>
-                <Grid item xs={2}>Allocated Amt</Grid>
-                <Grid item xs={2}>Current Bal</Grid>
+                <Grid item xs={1}><Typography variant="body1">Slno</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body1">Lab No</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body1">Date</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body1">Select</Typography></Grid>
+                <Grid item xs={2}><Typography variant="body1">Patient</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body1">Amount</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body1">Balance</Typography></Grid>
+                <Grid item xs={2}><Typography variant="body1">Allocated Amt</Typography></Grid>
+                <Grid item xs={2}><Typography variant="body1">Current Bal</Typography></Grid>
               </Grid>
+     
+              <Grid container spacing={2}>
+                <Grid item xs={1}><Typography variant="body2">1</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body2">1234</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body2">12-07-2024</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body2">Yes</Typography></Grid>
+                <Grid item xs={2}><Typography variant="body2">John Doe</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body2">$100</Typography></Grid>
+                <Grid item xs={1}><Typography variant="body2">$20</Typography></Grid>
+                <Grid item xs={2}><Typography variant="body2">$80</Typography></Grid>
+                <Grid item xs={2}><Typography variant="body2">$0</Typography></Grid>
+              </Grid>
+             
             </Box>
-          </Grid>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-               <Grid item xs={12} sm={12} md={6}>
-            <FormControl component="fieldset">
-              <RadioGroup row value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)}>
-                <FormControlLabel value="cash" control={<Radio />} label="Cash" />
-                <FormControlLabel value="cheque" control={<Radio />} label="Cheque" />
-                <FormControlLabel value="bhim" control={<Radio />} label="BHIM/UPI Online Payment" />
-              </RadioGroup>
-            </FormControl>
-          </Grid>
-          </Grid>
+          </Grid> */}
         </Grid>
       </Paper>
     </Container>
